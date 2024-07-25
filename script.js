47a16d00e69ca412c9981adfccff4db2077bd367
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-07-25 14:40:08 -0300

    update

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
- const api = fetch('http://localhost:3000/')
-.then( res => console.log(res.json()))
+ const containerVideos = document.querySelector('.videos__container')
+ 
+ const api = fetch('http://localhost:3000/videos')
+.then( res => res.json())
+.then((videos) =>
+    videos.forEach((video) => {
+    containerVideos.innerHTML +=`
+    <li class="videos__item">
+    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+    </li>`
+    }
+))
